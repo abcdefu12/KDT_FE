2ed0f30529a80caf1254d20ebba47bc6eae8db6a
Gim <gbks081@gmail.com>
2021-12-28 20:58:29 +0900

    config: change path to add vue content
    add folder name basic

---
+++ basic/bundler/webpack/main.js
@@ -0,0 +1,15 @@
+import 'main.scss'
+
+console.log('hello')
+
+
+
+// "dev": "parcel bundler/parcel/index.html",
+//     "build": "parcel build bundler/parcel/index.html"
+
+// "dev": "webpack-dev-server --mode development",
+//     "build": "webpack --mode production"
+
+// "webpack": "^5.65.0",
+//     "webpack-cli": "^4.9.1",
+//     "webpack-dev-server": "^4.0.0-rc.1"
